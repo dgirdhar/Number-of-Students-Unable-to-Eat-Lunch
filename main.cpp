dae83073e6126d5ea211adaf2593ae4f752806fe
Dhiraj Girdhar <girdhar.dhiraj@gmail.com>
2021-01-16 17:09:02 -0500

    Update main.cpp
    Solving the problem without erasing the content from the vector, got following results.
    
    Runtime: 0 ms, faster than 100.00% of C++ online submissions for Number of Students Unable to Eat Lunch.
    Memory Usage: 8.8 MB, less than 95.82% of C++ online submissions for Number of Students Unable to Eat Lunch.

--- main.cpp
+++ main.cpp
@@ -3,24 +3,27 @@ public:
     int countStudents(vector<int>& students, vector<int>& sandwiches) {
         int counter = 0;
         int res = 0;
+        int studentsCounter = 0;
+        int sanwichesCounter = 0;
+        int studentsSize = students.size();
         
-        while (counter < students.size()) {
-            if (students[0] == sandwiches[0]) {
-                students.erase(students.begin());
-                sandwiches.erase(sandwiches.begin());
+        while (counter < studentsSize && studentsCounter < students.size()) {
+            if (students[studentsCounter] == sandwiches[sanwichesCounter]) {
+                studentsCounter++;
+                sanwichesCounter++;
+                studentsSize--;
                 counter = 0;
             }
             else {
                 counter++;
-                int temp = students[0];
+                int temp = students[studentsCounter++];
                 
-                students.erase(students.begin());
                 students.push_back(temp);
             }
         }
         
-        res = students.size();
+        res = students.size() - studentsCounter;
         
-        return students.size();
+        return res;
     }
 };
